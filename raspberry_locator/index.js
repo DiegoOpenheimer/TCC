128722a50f94a1803dc2ed663936ddf1861902a4
Diego Alves Openheimer <diegoop.pa@hotmail.com>
2019-03-22 19:40:48 -0300

    Adicionado um delay para enviar a localização via servidor broker

--- raspberry_locator/index.js
+++ raspberry_locator/index.js
@@ -1,16 +1,28 @@
-const SerialPort = require('serialport');
-const GPS = require('gps');
+const SerialPort = require('serialport')
+const GPS = require('gps')
+let send = true
 
 const port = new SerialPort('/dev/ttyAMA0', {
   baudRate: 9200,
-});
+})
 
-const gps = new GPS;
+const gps = new GPS
+
  
-gps.on('data', function(data) {
-  console.log(data, gps.state);
-});
- 
-port.on('data', function(data) {
-  gps.updatePartial(data);
-});
+gps.on('data', data => {
+  publishLocation(data)
+})
+
+port.on('data', data => {
+  gps.updatePartial(data)
+})
+
+function publishLocation(location) {
+  if (send) {
+    send = false
+    setTimeout(() => {
+      console.log('try to send location', location)
+      send = true
+    }, 5000)
+  }
+}
