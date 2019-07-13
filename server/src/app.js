2ad70409c47637dfa7fb5a6e8e6e4a921dfc29a6
Diego Alves Openheimer <diegoop.pa@hotmail.com>
2019-07-12 22:31:00 -0300

    Adicionado lib de cors no servidor

--- server/src/app.js
+++ server/src/app.js
@@ -4,13 +4,14 @@ const bodyParser = require('body-parser')
 const swaggerUi = require('swagger-ui-express')
 const YAML = require('yamljs')
 const swaggerDocument = YAML.load('./swagger/swagger.yaml')
+const cors = require('cors')
 
 app.set('views', './src/views')
 app.set('view engine', 'ejs')
-app.use('/api-doc', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 app.use(bodyParser.json({extended: true}))
 app.use(bodyParser.urlencoded({extended: true}))
-
+app.use(cors())
 app.use(require('./routes'))
 
 module.exports = app
