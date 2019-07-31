e2c1eb2565d740f73768d89fc30daaf28d37f0d2
Diego Alves Openheimer <diegoop.pa@hotmail.com>
2019-07-30 21:45:55 -0300

    Removido pasta node modules do projeto rasp

--- raspberry_locator/index.js
+++ raspberry_locator/index.js
@@ -1,31 +1,31 @@
-// const SerialPort = require('serialport')
-// const GPS = require('gps')
-// let send = true
+const SerialPort = require('serialport')
+const GPS = require('gps')
+let send = true
 
-// const port = new SerialPort('/dev/ttyAMA0', {
-//   baudRate: 9200,
-// })
+const port = new SerialPort('/dev/ttyAMA0', {
+  baudRate: 9200,
+})
 
-// const gps = new GPS
+const gps = new GPS
 
  
-// gps.on('data', data => {
-//   publishLocation(data)
-// })
+gps.on('data', data => {
+  publishLocation(data)
+})
 
-// port.on('data', data => {
-//   gps.updatePartial(data)
-// })
+port.on('data', data => {
+  gps.updatePartial(data)
+})
 
-// function publishLocation(location) {
-//   if (send) {
-//     send = false
-//     setTimeout(() => {
-//       console.log('try to send location', location)
-//       send = true
-//     }, 5000)
-//   }
-// }
+function publishLocation(location) {
+  if (send) {
+    send = false
+    setTimeout(() => {
+      console.log('try to send location', location)
+      send = true
+    }, 5000)
+  }
+}
 
 const si = require('systeminformation')
 
