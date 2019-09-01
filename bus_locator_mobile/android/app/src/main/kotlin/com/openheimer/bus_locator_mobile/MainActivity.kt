e69077723b2370e18d760c46c0affd6d3b9d74ee
Diego Alves Openheimer <diegoop.pa@hotmail.com>
2019-09-01 20:14:47 -0300

    Adicionado projeto mobile no repositório

---
+++ bus_locator_mobile/android/app/src/main/kotlin/com/openheimer/bus_locator_mobile/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.openheimer.bus_locator_mobile
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
